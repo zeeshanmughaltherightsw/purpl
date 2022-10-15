2c2d754f7d16c4180a876c6f13c1c2a9f36d84fb
= <=>
2022-10-15 17:18:46 +0500

    home page uploaded

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -6,7 +6,7 @@ import { Head, Link } from '@inertiajs/inertia-vue3';
     <Head title="Welcome" />
 
     <nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top p-0 shadow">
-        <a href="index.html" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
+        <a href="#" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
             <h1 class="m-0">Pr Pal</h1>
         </a>
         <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
